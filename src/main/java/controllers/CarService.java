11abc961796861af14feeeade1a1ddbb0288d738
Natalia <tankova.natalya@bk.ru>
2022-02-28 17:16:10 +0800

    r

--- src/main/java/controllers/CarService.java
+++ src/main/java/controllers/CarService.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
-import sun.util.resources.LocaleData;
 
 import java.time.LocalDate;
 
